0f02431e80fc35962e9a8019f693c8b1b23fdf9a
Liang <392425595@qq.com>
2020-06-03 09:18:18 +0800

    :lipstick: Updating Button style

--- src/App.tsx
+++ src/App.tsx
@@ -4,27 +4,12 @@ import Button, {ButtonSiz, ButtonType} from './components/Button/button';
 function App() {
   return (
     <div className="App">
-      <Button disable>Hello</Button>
-      <Button btnType={ButtonType.Primary} size={ButtonSiz.Large}>Hello</Button>
-      <Button btnType={ButtonType.Link} href="http://www.baidu.com">Baidu</Button>
-      <header className="App-header">
-        <h1>Hello H1</h1>
-        <h2>Hello H2</h2>
-        <h3>Hello H3</h3>
-        <h4>Hello H4</h4>
-        <p>
-          Edit <code>src/App.tsx</code> and save to reload.
-        </p>
-        <hr/>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+      <Button>Hello</Button>
+      <Button disable>Disabled Button</Button>
+      <Button btnType={ButtonType.Primary} size={ButtonSiz.Large}>Large Primary Button</Button>
+      <Button btnType={ButtonType.Danger} size={ButtonSiz.Small}>Small Danger Button</Button>
+      <Button btnType={ButtonType.Link} href="#">Link Button</Button>
+      <Button btnType={ButtonType.Link} size={ButtonSiz.Small} disable>Small Danger Link</Button>
     </div>
   );
 }
